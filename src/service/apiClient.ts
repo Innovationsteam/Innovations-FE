1f84ee6e4eb7419551798e6983ae6f7e0b703462
Gideon Ogordi <gideonogordi@gmail.com>
2024-11-03 12:18:17 +0100

    merged post

--- src/service/apiClient.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const token = sessionStorage.getItem("token");
-export const axiosInstance = axios.create({
-	baseURL: "https://christianwritesbe.onrender.com",
-	// withCredentials: true
-});
