8ff9c0ceb23bfc177057f6f75248425a42222e3a
johnsonearly <johnsonegbichi@gmail.com>
2025-01-08 11:14:58 +0100

    Edited the sign up message and commented out the forgot password

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -8,7 +8,7 @@ const SignUp = () => {
 		<PageContainer className="flex h-screen items-center justify-center md:bg-[#F5F5F5]">
 			<Container className="!h-fit max-w-[600px] bg-white p-8 font-roboto md:rounded-xl md:shadow-lg">
 				<h1 className="text-3xl font-bold sm:text-4xl">Create an Account</h1>
-				<p className="mt-1 text-sm text-[#242424B2] sm:text-base">Empower. Create. Discover. Connect. Thrive</p>
+				<p className="mt-1 text-sm text-[#242424B2] sm:text-base">Join a community of Christian creatives today</p>
 				<SignUpForm />
 				<p className="mt-5 text-center">
 					Already have an account?{" "}
